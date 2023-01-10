18e34595c700186a99f932d7da1b2dc50a6c966f
maitian352 <68207888+maitian352@users.noreply.github.com>
2023-01-09 21:15:09 -0500

    a

--- src/botmaitian/RobotPlayer.java
+++ src/botmaitian/RobotPlayer.java
@@ -1,4 +1,4 @@
-package bot;
+package botmaitian;
 
 import battlecode.common.*;
 
@@ -9,29 +9,9 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
-/**
- * RobotPlayer is the class that describes your main robot strategy.
- * The run() method inside this class is like your main function: this is what we'll call once your robot
- * is created!
- */
-public strictfp class RobotPlayer {
-
-    /**
-     * We will use this variable to count the number of turns this robot has been alive.
-     * You can use static variables like this to save any information you want. Keep in mind that even though
-     * these variables are static, in Battlecode they aren't actually shared between your robots.
-     */
+public strictfp class RobotPlayer{
     static int turnCount = 0;
-
-    /**
-     * A random number generator.
-     * We will use this RNG to make some random moves. The Random class is provided by the java.util.Random
-     * import at the top of this file. Here, we *seed* the RNG with a constant number (6147); this makes sure
-     * we get the same sequence of numbers every time this code is run. This is very useful for debugging!
-     */
     static final Random rng = new Random(6147);
-
-    /** Array containing all the possible movement directions. */
     static final Direction[] directions = {
         Direction.NORTH,
         Direction.NORTHEAST,
@@ -42,16 +22,8 @@ public strictfp class RobotPlayer {
         Direction.WEST,
         Direction.NORTHWEST,
     };
-
-    /**
-     * run() is the method that is called when a robot is instantiated in the Battlecode world.
-     * It is like the main function for your robot. If this method returns, the robot dies!
-     *
-     * @param rc  The RobotController object. You use it to perform actions from this robot, and to get
-     *            information on its current status. Essentially your portal to interacting with the world.
-     **/
     @SuppressWarnings("unused")
-    public static void run(RobotController rc) throws GameActionException {
+    public static void run(RobotController rc) throws GameActionException{
 
         // Hello world! Standard output is very useful for debugging.
         // Everything you say here will be directly viewable in your terminal when you run a match!
@@ -60,7 +32,7 @@ public strictfp class RobotPlayer {
         // You can also use indicators to save debug notes in replays.
         rc.setIndicatorString("Hello world!");
 
-        while (true) {
+        while(true){
             // This code runs during the entire lifespan of the robot, which is why it is in an infinite
             // loop. If we ever leave this loop and return from run(), the robot dies! At the end of the
             // loop, we call Clock.yield(), signifying that we've done everything we want to do.
@@ -105,11 +77,6 @@ public strictfp class RobotPlayer {
 
         // Your code should never reach here (unless it's intentional)! Self-destruction imminent...
     }
-
-    /**
-     * Run a single turn for a Headquarters.
-     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
-     */
     static void runHeadquarters(RobotController rc) throws GameActionException {
         WellInfo[] wells = rc.senseNearbyWells();
         MapLocation me = rc.getLocation();
@@ -141,12 +108,7 @@ public strictfp class RobotPlayer {
             }
         }
     }
-
-    /**
-     * Run a single turn for a Carrier.
-     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
-     */
-    static void runCarrier(RobotController rc) throws GameActionException {
+    static void runCarrier(RobotController rc) throws GameActionException{
         if (rc.getAnchor() != null) {
             // If I have an anchor singularly focus on getting it to the first island I see
             int[] islands = rc.senseNearbyIslands();
@@ -187,7 +149,7 @@ public strictfp class RobotPlayer {
             }
         }
         // Occasionally try out the carriers attack
-        if (rng.nextInt(20) == 1) {
+        if(rng.nextInt(20) == 1){
             RobotInfo[] enemyRobots = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
             if (enemyRobots.length > 0) {
                 if (rc.canAttack(enemyRobots[0].location)) {
@@ -198,7 +160,7 @@ public strictfp class RobotPlayer {
         
         // If we can see a well, move towards it
         WellInfo[] wells = rc.senseNearbyWells();
-        if (wells.length > 1 && rng.nextInt(3) == 1) {
+        if (wells.length > 1 && rng.nextInt(3) == 1){
             WellInfo well_one = wells[1];
             Direction dir = me.directionTo(well_one.getMapLocation());
             if (rc.canMove(dir)) 
@@ -206,16 +168,11 @@ public strictfp class RobotPlayer {
         }
         // Also try to move randomly.
         Direction dir = directions[rng.nextInt(directions.length)];
-        if (rc.canMove(dir)) {
+        if(rc.canMove(dir)){
             rc.move(dir);
         }
     }
-
-    /**
-     * Run a single turn for a Launcher.
-     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
-     */
-    static void runLauncher(RobotController rc) throws GameActionException {
+    static void runLauncher(RobotController rc) throws GameActionException{
         // Try to attack someone
         int radius = rc.getType().actionRadiusSquared;
         Team opponent = rc.getTeam().opponent();
